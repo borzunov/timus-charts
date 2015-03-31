4bfb6caa3553bf36f602441a607403f1bf4bbde5
Alexander Borzunov <hxrussia@gmail.com>
2015-03-31 19:47:06 +0500

    Fix some jshint issues

--- src/timus.user.js
+++ src/timus.user.js
@@ -15,23 +15,25 @@
 // @require      http://cdnjs.cloudflare.com/ajax/libs/spin.js/2.0.1/spin.min.js
 // ==/UserScript==
 
+/* jshint multistr: true */
+
 (function () {
     /* Engine-dependent functions */
-    
+
     var isFirefox = (navigator.userAgent.indexOf('Firefox') !== -1);
-    
+
     function getValue(key) {
         var value;
         if (isFirefox)
             value = GM_getValue(key);
         else
             value = localStorage[key];
-        
+
         if (value === undefined)
             throw new Error("Storage doesn't contain this key");
         return value;
     }
-        
+
     function setValue(key, value) {
         try {
             if (isFirefox)
@@ -49,7 +51,7 @@
     var style = document.createElement('style');
     style.textContent = '.jqplot-target{position:relative;color:#666;font-family:"Trebuchet MS",Arial,Helvetica,sans-serif;font-size:1em;}.jqplot-axis{font-size:.75em;}.jqplot-xaxis{margin-top:10px;}.jqplot-x2axis{margin-bottom:10px;}.jqplot-yaxis{margin-right:10px;}.jqplot-y2axis,.jqplot-y3axis,.jqplot-y4axis,.jqplot-y5axis,.jqplot-y6axis,.jqplot-y7axis,.jqplot-y8axis,.jqplot-y9axis,.jqplot-yMidAxis{margin-left:10px;margin-right:10px;}.jqplot-axis-tick,.jqplot-xaxis-tick,.jqplot-yaxis-tick,.jqplot-x2axis-tick,.jqplot-y2axis-tick,.jqplot-y3axis-tick,.jqplot-y4axis-tick,.jqplot-y5axis-tick,.jqplot-y6axis-tick,.jqplot-y7axis-tick,.jqplot-y8axis-tick,.jqplot-y9axis-tick,.jqplot-yMidAxis-tick{position:absolute;white-space:pre;}.jqplot-xaxis-tick{top:0;left:15px;vertical-align:top;}.jqplot-x2axis-tick{bottom:0;left:15px;vertical-align:bottom;}.jqplot-yaxis-tick{right:0;top:15px;text-align:right;}.jqplot-yaxis-tick.jqplot-breakTick{right:-20px;margin-right:0;padding:1px 5px 1px 5px;z-index:2;font-size:1.5em;}.jqplot-y2axis-tick,.jqplot-y3axis-tick,.jqplot-y4axis-tick,.jqplot-y5axis-tick,.jqplot-y6axis-tick,.jqplot-y7axis-tick,.jqplot-y8axis-tick,.jqplot-y9axis-tick{left:0;top:15px;text-align:left;}.jqplot-yMidAxis-tick{text-align:center;white-space:nowrap;}.jqplot-xaxis-label{margin-top:10px;font-size:11pt;position:absolute;}.jqplot-x2axis-label{margin-bottom:10px;font-size:11pt;position:absolute;}.jqplot-yaxis-label{margin-right:10px;font-size:11pt;position:absolute;}.jqplot-yMidAxis-label{font-size:11pt;position:absolute;}.jqplot-y2axis-label,.jqplot-y3axis-label,.jqplot-y4axis-label,.jqplot-y5axis-label,.jqplot-y6axis-label,.jqplot-y7axis-label,.jqplot-y8axis-label,.jqplot-y9axis-label{font-size:11pt;margin-left:10px;position:absolute;}.jqplot-meterGauge-tick{font-size:.75em;color:#999;}.jqplot-meterGauge-label{font-size:1em;color:#999;}table.jqplot-table-legend{margin-top:12px;margin-bottom:12px;margin-left:12px;margin-right:12px;}table.jqplot-table-legend,table.jqplot-cursor-legend{background-color:rgba(255,255,255,0.6);border:1px solid #ccc;position:absolute;font-size:.75em;}td.jqplot-table-legend{vertical-align:middle;}td.jqplot-seriesToggle:hover,td.jqplot-seriesToggle:active{cursor:pointer;}.jqplot-table-legend .jqplot-series-hidden{text-decoration:line-through;}div.jqplot-table-legend-swatch-outline{border:1px solid #ccc;padding:1px;}div.jqplot-table-legend-swatch{width:0;height:0;border-top-width:5px;border-bottom-width:5px;border-left-width:6px;border-right-width:6px;border-top-style:solid;border-bottom-style:solid;border-left-style:solid;border-right-style:solid;}.jqplot-title{top:0;left:0;padding-bottom:.5em;font-size:1.2em;}table.jqplot-cursor-tooltip{border:1px solid #ccc;font-size:.75em;}.jqplot-cursor-tooltip{border:1px solid #ccc;font-size:.75em;white-space:nowrap;background:rgba(208,208,208,0.5);padding:1px;}.jqplot-highlighter-tooltip,.jqplot-canvasOverlay-tooltip{border:1px solid #ccc;font-size:.75em;white-space:nowrap;background:rgba(208,208,208,0.5);padding:1px;}.jqplot-point-label{font-size:.75em;z-index:2;}td.jqplot-cursor-legend-swatch{vertical-align:middle;text-align:center;}div.jqplot-cursor-legend-swatch{width:1.2em;height:.7em;}.jqplot-error{text-align:center;}.jqplot-error-message{position:relative;top:46%;display:inline-block;}div.jqplot-bubble-label{font-size:.8em;padding-left:2px;padding-right:2px;color:rgb(20%,20%,20%);}div.jqplot-bubble-label.jqplot-bubble-label-highlight{background:rgba(90%,90%,90%,0.7);}div.jqplot-noData-container{text-align:center;background-color:rgba(96%,96%,96%,0.3);}';
     document.body.appendChild(style);
-    
+
     var STYLE_CONTENT = 'TABLE.attempt_list TR TD.cmpac {\
     background-color: #f99 !important;\
 }\
@@ -166,13 +168,13 @@
 .chart_version {\
     float: right;\
 }';
-    
-    function applyStyle() {        
+
+    function applyStyle() {
         var style = document.createElement('style');
         style.textContent = STYLE_CONTENT;
         document.head.appendChild(style);
     }
-    
+
     applyStyle();
 
 
@@ -215,14 +217,14 @@
     }
 
     var locale = LOCALES[isRussianLocale() ? "ru" : "en"];
-    
-    
+
+
     function Submit() {}
-    
+
     // This dictionary duplicates some "problem*_*" records of local storage
     // because it can be full or inaccessible
     Submit.problemsFromContests = {};
-    
+
     Submit.prototype.checkWhetherConsidered = function (callback) {
         if (this.verdict !== 'Accepted') {
             callback(false);
@@ -232,12 +234,12 @@
             callback(true);
             return;
         }
-        
+
         // Check whether the online-contest problem is
         // copied to the main archive
         var problemID = this.space + ',' + this.problemNo;
         var cacheKey = 'problem' + this.space + '_' + this.problemNo;
-        var archiveNo;        
+        var archiveNo;
         if (problemID in Submit.problemsFromContests) {
             archiveNo = Submit.problemsFromContests[problemID];
         } else {
@@ -255,7 +257,7 @@
                 callback(false);
             return;
         }
-        
+
         var address = 'http://acm.timus.ru/problem.aspx?space=' +
                 this.space + '&num=' + this.problemNo;
         var submit = this;
@@ -265,20 +267,20 @@
                 var archiveNo = match[2];
                 Submit.problemsFromContests[problemID] = archiveNo;
                 setValue(cacheKey, archiveNo);
-                
+
                 submit.space = 1;
                 submit.problemNo = archiveNo;
                 callback(true);
             } else {
                 Submit.problemsFromContests[problemID] = "null";
                 setValue(cacheKey, "null");
-                
+
                 callback(false);
             }
         });
     };
-    
-    
+
+
     function Author(judgeID) {
         this.judgeID = judgeID;
         this.acceptedProblems = {};
@@ -287,8 +289,8 @@
                 judgeID + '&status=accepted';
         this.lastSubmitID = null;
         this.noMorePages = false;
-    };
-    
+    }
+
     Author.prototype.saveToCache = function () {
         var keyPrefix = 'author' + this.judgeID;
         setValue(keyPrefix + '_acceptedProblems',
@@ -298,7 +300,7 @@
         setValue(keyPrefix + '_lastKnownSubmitID',
                 this.lastSubmitID);
     };
-    
+
     Author.prototype.loadFromCache = function () {
         var keyPrefix = 'author' + this.judgeID;
         try {
@@ -313,7 +315,7 @@
             this.cacheAvailable = false;
         }
     };
-    
+
     Author.prototype.retrieve = function (
             expectedAcceptedProblemsCount, callback) {
         this.loadFromCache();
@@ -321,16 +323,16 @@
                 expectedAcceptedProblemsCount) {
             this.acceptedProblemsCount = this.cachedAcceptedProblemsCount;
             this.lastSubmitID = this.cachedLastSubmitID;
-            
+
             if (callback !== undefined)
                 callback();
             return;
         }
-        
+
         this.retrieveCallback = callback;
         this.parsePage(null);
     };
-    
+
     Author.prototype.parsePage = function (fromSubmitID) {
         var submitsQueried = (this.cacheAvailable ? 200 : 1000);
         var address = this.submitsAddress + '&count=' + submitsQueried;
@@ -358,14 +360,14 @@
             author.processSubmit(0);
         });
     };
-    
+
     var MSEC_PER_SEC = 1e3;
-    
+
     Author.prototype.processSubmit = function (index) {
         if (index === this.submits.length) {
             if (this.noMorePages) {
                 this.saveToCache();
-                
+
                 if (this.retrieveCallback !== undefined)
                     this.retrieveCallback();
             } else
@@ -378,12 +380,12 @@
         if (this.cacheAvailable && submit.id == this.cachedLastSubmitID) {
             this.acceptedProblemsCount += this.cachedAcceptedProblemsCount;
             this.saveToCache();
-                        
+
             if (this.retrieveCallback !== undefined)
                 this.retrieveCallback();
             return;
         }
-        
+
         var author = this;
         submit.checkWhetherConsidered(function (isConsidered) {
             if (isConsidered &&
@@ -399,15 +401,15 @@
             author.processSubmit(index + 1);
         });
     };
-    
-    
+
+
     function Line(author, name, color) {
         this.author = author;
         this.rowID = 'chart_user_' + author.judgeID;
         this.name = name;
         this.color = color;
     }
-    
+
     Line.prototype.make = function () {
         var dates = [];
         for (var key in this.author.acceptedProblems) {
@@ -419,24 +421,24 @@
             return a - b;
         });
         this.points = [];
-        for (var i = 0; i < dates.length; i++) 
+        for (var i = 0; i < dates.length; i++)
             this.points.push([new Date(dates[i]), i + 1]);
     };
-    
-    
+
+
     function substTemplateVariables(template, variables) {
         for (var name in variables)
             template = template.replace(
                     new RegExp('\{% ' + name + ' %\}', 'g'), variables[name]);
         return template;
     }
-    
+
     var COLOR_GREEN = '#4f4';
     var COLOR_RED = '#f77';
     var COLOR_BLUE = '#77f';
 
     var TEMPLATE_TOGGLE_LINK = '<br /><a href="#" class="chart_toggle">{% label %}</a>';
-    
+
     var TEMPLATE_USER_BEGIN = '<tr id="{% row_id %}">\
     <td><div class="chart_user_color" style="background: {% color %};"></div></td>\
     <td class="chart_user_judge_id">{% judge_id %}</td>\
@@ -444,7 +446,7 @@
     var TEMPLATE_USER_SEVERAL_LINES = '<td class="chart_user_problems_count">{% problems_count %}</td>\
 <td><a href="#" class="chart_user_delete">' + locale.del + '</a></td>';
     var TEMPLATE_USER_END = '</tr>';
-    
+
     var TEMPLATE_CHART = '<div id="chart_place" style="display: none;">\
     <div id="chart_loading" class="chart_box">\
         <div class="chart_comment chart_version">\
@@ -468,18 +470,18 @@
         </div>\
     </div>\
 </div>';
-    
+
     function Chart() {
         this.ready = false;
         this.visible = false;
-        
+
         this.lines = [];
         this.linesExpected = 0;
-        
+
         this.animateComplete = false;
         this.loadingState = false;
     }
-    
+
     Chart.prototype.loading = function (state) {
         if (state === false) {
             this.spinner.stop();
@@ -497,14 +499,14 @@
         }
         return this.loadingState;
     };
-    
+
     Chart.prototype.sortLines = function () {
         this.lines.sort(function (a, b) {
             return b.author.acceptedProblemsCount -
                     a.author.acceptedProblemsCount;
         });
     };
-    
+
     Chart.prototype.redrawLegend = function () {
         var severalLines = this.lines.length > 1;
         var code = '';
@@ -529,7 +531,7 @@
                 var curLine = this.lines[i];
                 $('#' + curLine.rowID + ' .chart_user_delete').click(
                     (function (judgeID) {
-                        return function (event) {                            
+                        return function (event) {
                             chart.removeUser(judgeID);
                             event.preventDefault();
                         };
@@ -538,10 +540,10 @@
             }
         }
     };
-    
+
     var CHART_YAXIS_SMALLEST_MAX = 10;
     var CHART_XAXIS_CRITICAL_DIFF = 1000 * 60 * 60 * 24 * 3;
-    
+
     Chart.prototype.fixPlot = function (plot) {
         var needReplot = false;
         // Fix non-integer ticks on X axis when maximal value is small
@@ -559,12 +561,12 @@
         }
         if (needReplot)
             plot.replot();
-    }
-    
+    };
+
     Chart.prototype.redraw = function () {
         this.sortLines();
         this.redrawLegend();
-        
+
         var points = [];
         var colors = [];
         for (var i = 0; i < this.lines.length; i++) {
@@ -574,7 +576,7 @@
         }
         points.reverse();
         colors.reverse();
-        
+
         $('.chart_legend_box').show();
         $('#chart').html('');
         this.loading(false);
@@ -604,22 +606,22 @@
         });
         this.fixPlot(plot);
     };
-    
+
     function zfill(str, width) {
         while (str.length < width)
             str = '0' + str;
         return str;
     }
-    
+
     function randomColor() {
         return '#' + zfill(Math.floor(Math.random() * 0x1000).toString(16), 3);
     }
-    
+
     Chart.prototype.showJudgeIDError = function (message) {
         this.loading(false);
         $('.chart_error_judge_id').html(message).slideDown(50);
     };
-    
+
     Chart.prototype.addUser = function (
             judgeID, name, color, deleteButton,
             expectedAcceptedProblemsCount, callback) {
@@ -635,7 +637,7 @@
                 chart.showJudgeIDError(locale.judgeIDNotEnoughOfAccepted);
                 return;
             }
-                
+
             chart.lines.push(line);
             if (chart.lines.length === chart.linesExpected) {
                 if (chart.animateComplete)
@@ -644,8 +646,8 @@
                     callback();
             }
         });
-    }
-    
+    };
+
     Chart.prototype.removeUser = function (judgeID) {
         for (var i = 0; i < this.lines.length; i++)
             if (this.lines[i].author.judgeID === judgeID) {
@@ -654,8 +656,8 @@
                 this.redraw();
                 break;
             }
-    }
-    
+    };
+
     Chart.prototype.loadJudgeID = function () {
         if (this.loading())
             return;
@@ -669,23 +671,23 @@
             return;
         }
         judgeID = match[1];
-        
+
         for (var i = 0; i < this.lines.length; i++)
-            if (this.lines[i].author.judgeID === judgeID) {                    
+            if (this.lines[i].author.judgeID === judgeID) {
                 this.showJudgeIDError(locale.judgeIDIsAlreadyAdded);
                 return;
             }
-        
+
         var address = 'http://acm.timus.ru/author.aspx?id=' + judgeID;
         var chart = this;
         $.get(address, function (data) {
             var match = /<H2 CLASS="author_name">(<A HREF=".*?" TARGET="_blank">)?(.+?)(<\/A>)?<\/H2>/i.exec(data);
-            if (match === null) {                    
+            if (match === null) {
                 chart.showJudgeIDError(locale.judgeIDDoesntExists);
                 return;
             }
             var name = match[2];
-            
+
             var color = $('#chart_new_user_color').css('background-color');
             chart.addUser(judgeID, name, color, true, null, function () {
                 $('.chart_judge_id_input').val('');
@@ -694,17 +696,17 @@
             });
         });
     };
-    
-    Chart.prototype.showLegend = function () {        
+
+    Chart.prototype.showLegend = function () {
         $('.chart_legend_open').hide();
         $('.chart_legend').slideDown(150);
         $('.chart_judge_id_input').focus();
     };
-    
+
     Chart.prototype.areEnoughDataPresent = function () {
         this.matchId = /id=(\d+)/i.exec(location.href);
         this.matchCompareto = /compareto=(\d+)/i.exec(location.href);
-        
+
         // Handle empty accounts
         if (this.matchCompareto !== null) {
             var bothCount = $('td.both').length - 1;
@@ -719,7 +721,7 @@
         }
         return true;
     };
-    
+
     Chart.prototype.createToggleLink = function (expectedVisibility) {
         var label = expectedVisibility ? locale.hideChart : locale.showChart;
         $('.author_links')
@@ -735,7 +737,7 @@
             event.preventDefault();
         });
     };
-    
+
     Chart.prototype.createChartPlace = function () {
         var chart = this;
         $('.author_links').after(TEMPLATE_CHART);
@@ -756,10 +758,10 @@
             chart.loadJudgeID();
             event.preventDefault();
         });
-        
+
         this.spinner = new Spinner();
     };
-    
+
     Chart.prototype.loadInitialData = function () {
         var profileName;
         var link = $('h2.author_name a');
@@ -767,7 +769,7 @@
             profileName = link.html();
         else
             profileName = $('h2.author_name').html();
-        if (this.matchCompareto !== null) {            
+        if (this.matchCompareto !== null) {
             var otherName =
                     $('.author_comparison_legend .padright:first').html();
             if (this.greenCount >= 2)
@@ -780,23 +782,23 @@
             this.addUser(this.matchId[1], profileName, COLOR_GREEN,
                     false, this.problemsCount);
     };
-    
+
     Chart.prototype.hide = function () {
         if (!this.visible)
             return;
         this.visible = false;
         setValue('chart_visible', 0);
-        
+
         $('.chart_toggle').html(locale.showChart);
         $('#chart_place').slideUp(300);
     };
-    
+
     Chart.prototype.show = function () {
         if (this.visible)
             return;
         this.visible = true;
         setValue('chart_visible', 1);
-        
+
         $('.chart_toggle').html(locale.hideChart);
         if (!this.ready)
             this.createChartPlace();
@@ -814,7 +816,7 @@
                     chart.redraw();
             });
     };
-    
+
     Chart.prototype.getDefaultVisibility = function () {
         try {
             if (getValue('chart_visible') == 0)
@@ -822,7 +824,7 @@
         } catch (err) {}
         return true;
     };
-    
+
     var chart = new Chart();
     if (chart.areEnoughDataPresent()) {
         var expectedVisibility = chart.getDefaultVisibility();
