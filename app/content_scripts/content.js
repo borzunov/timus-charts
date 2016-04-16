7051049972525b1fb888c273e1849bf16f269675
Slava Shklyaev <slava@slava.sh>
2016-04-16 16:00:27 +0300

    Mark recent ACs

--- app/content_scripts/content.js
+++ app/content_scripts/content.js
@@ -3,9 +3,22 @@
     if (chart.areEnoughDataPresent()) {
         var expectedVisibility = chart.getDefaultVisibility();
         chart.createToggleLink(expectedVisibility);
-        if (expectedVisibility)
+        if (expectedVisibility) {
             $(function () {
                 chart.show();
             });
+        }
+        if (chart.matchCompareto === null) {
+            $(function () {
+                var times = chart.lines[0].author.acceptedProblems;
+                Object.keys(times).sort(function(a, b) {
+                    return times[b] - times[a];
+                }).forEach(function (probId, i) {
+                    var td = $('.attempt_list td.accepted:contains("' + probId + '")');
+                    var hue = 120 + (1 - Math.min(1, i / 60)) * 60;
+                    td.css('background-color', 'hsl(' + hue + ', 100%, 78%)');
+                });
+            });
+        }
     }
 })();
